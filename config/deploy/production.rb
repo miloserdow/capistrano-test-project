bd614c2ee23ee7f557e24f718c9022f97d04f82a
Vladimir Miloserdov <milosvova@gmail.com>
2024-05-26 14:07:35 +0100

    Add secrets

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,3 @@
-server 'your-server-ip-or-domain', user: 'deploy', roles: %w{app db web}
+server '${{ secrets.SERVER_IP }}', user: '${{ secrets.SERVER_USER }}', roles: %w{app db web}
 
 set :branch, 'main'
